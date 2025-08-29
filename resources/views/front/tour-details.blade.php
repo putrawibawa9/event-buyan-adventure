4c1fb021f39866f6d131560c9ed1458e8c6078f5
putrawibawa9 <putrawibawa7@gmail.com>
2025-08-29 15:32:05 +0800

    done

--- resources/views/front/tour-details.blade.php
+++ resources/views/front/tour-details.blade.php
@@ -16,10 +16,10 @@ $page_title = "Gallery";
                         <!--=== Single Gallery Item ===-->
                         <div class="single-gallery-item mb-30 wow fadeInUp">
                             <div class="gallery-img">
-                               <img src="{{ asset('uploads/' . $image->path) }}" alt="Gallery Image">
+                               <img src="{{ asset('public/uploads/' . $image->path) }}" alt="Gallery Image">
 
 <div class="hover-overlay">
-    <a href="{{ asset('uploads/' . $image->path) }}" class="icon-btn img-popup">
+    <a href="{{ asset('public/uploads/' . $image->path) }}" class="icon-btn img-popup">
         <i class="far fa-plus"></i>
     </a>
 </div>
